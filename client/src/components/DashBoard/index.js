d5b0f31e750142967cee265f00a97dd69b0ed615
Lelouch <shinydark.sky@gmail.com>
2021-10-09 14:23:14 +0700

    add type animal species

--- client/src/components/DashBoard/index.js
+++ client/src/components/DashBoard/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import AnimalBoard from './AnimalBoard'
+import AnimalSpecies from './AnimalSpecies'
+import Setting from './Setting'
 import './style.scss'
 export default function DashBoard({boardType}) {
     return (
         <div>
             {boardType == 0 && <AnimalBoard />}
+            {boardType == 1 && <AnimalSpecies />}
+            {boardType == 2 && <Setting />}
         </div>
     )
 }
